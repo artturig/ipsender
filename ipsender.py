5c6cc54957698c167018876bb53389d6e0526c99
Artturi Ala-Kauttu <artturi@geniem.com>
2016-08-11 13:04:13 +0300

    more logic
    dont send email if the ip has not changed

--- ipsender.py
+++ ipsender.py
@@ -2,8 +2,23 @@ import os
 import urllib2
 import json
 
-response = urllib2.urlopen('https://api.ipify.org?format=json')
-data = json.load(response)   
-print data
+# read latest ip from file
+ip1 = open('ip.txt', 'r').read()
+#print ip1
 
-os.system("echo \" %s \" | mail -s \"mokkula\" CHANGEME@email.fi" % data)
+# get current ip
+response = urllib2.urlopen('https://api.ipify.org?format=json')
+ip2 = json.load(response)
+ip2 = ip2['ip']
+#print ip2
+
+# compare ip's and send email if ip has changed
+if ip1 == ip2:
+    1 == 1
+    #print "they are the same"
+else:
+    f = open("ip.txt","w")
+    f.write(str(ip2))
+    f.close()
+    #print "different"
+    os.system("echo \" %s \" | mail -s \"mokkula\" artturi@saunalahti.fi" % ip2)
